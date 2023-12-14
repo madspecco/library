116047128badd1b88fc0c79a1e35f401c2365fe4
madspecco <bogdan.bolos30@gmail.com>
2023-12-14 14:33:05 +0200

    created button for adding a book

--- script.js
+++ script.js
@@ -7,11 +7,7 @@ function Book(title, author, pages, read)
     this.pages = pages;
     this.isRead = function()
     {
-        if(read)
-        {
-            return "read.";
-        }
-        return "not read yet."
+        return this.read ? "read." : "not read yet.";
     }
 
     this.info = function() 
@@ -33,7 +29,7 @@ addBookToLibrary("Crime and Punishment", "F.Dostoyevsky", 504, false);
 function displayBooks() {
     const bookContainer = document.getElementById("bookContainer");
 
-    // Clear existing content
+    // clear existing content
     bookContainer.innerHTML = "";
 
     for (const book of myLibrary) {
@@ -49,10 +45,33 @@ function displayBooks() {
             <p>Status: ${book.isRead()}</p>
         `;
 
-        // Append the card to the bookContainer
+        // add card to the bookContainer
         bookContainer.appendChild(card);
     }
 }
 
-// Display books when the page loads
+
+// function that adds a book based on user input
+function addBookPrompt() {
+    const title = prompt("Enter title:");
+    const author = prompt("Enter author:");
+    const pages = parseInt(prompt("Enter pages:"));
+    const read = confirm("Did you read this?");
+
+    if(title && author && !isNaN(pages)) {
+        addBookToLibrary(title, author, pages, read);
+        displayBooks();
+    }
+    else {
+        alert("Invalid input.");
+    }
+}
+
+// display books when the page loads
 window.addEventListener("load", displayBooks);
+
+
+// event listener for the add button
+document.getElementById("addBook").addEventListener("click", function() {
+    addBookPrompt();
+});
